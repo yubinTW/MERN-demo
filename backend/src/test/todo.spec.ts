7aded5b9ef2ae182ed52fee24a05e6ce97a40abe
yubintw <yubin.cs05g@nctu.edu.tw>
2021-11-19 01:53:13 +0800

    add .ready() before run test cases

--- backend/src/test/todo.spec.ts
+++ backend/src/test/todo.spec.ts
@@ -2,16 +2,16 @@ import { FastifyInstance } from 'fastify'
 import { startFastify } from '../server'
 import { Server, IncomingMessage, ServerResponse } from 'http'
 import * as dbHandler from './db'
-import * as E from 'fp-ts/Either'
 import { ITodo } from '../types/todo'
-import { constTrue } from 'fp-ts/lib/function'
 
-describe('Form test', () => {
+describe('Todo test', () => {
   let server: FastifyInstance<Server, IncomingMessage, ServerResponse>
+  const fastifyPort = 8888
 
   beforeAll(async () => {
     await dbHandler.connect()
-    server = startFastify(8888)
+    server = startFastify(fastifyPort)
+    await server.ready()
   })
 
   afterEach(async () => {
@@ -19,18 +19,9 @@ describe('Form test', () => {
   })
 
   afterAll(async () => {
-    E.match(
-      (e) => console.log(e),
-      (_) => console.log('Closing Fastify server is done!')
-    )(
-      E.tryCatch(
-        () => {
-          dbHandler.closeDatabase()
-          server.close((): void => {})
-        },
-        (reason) => new Error(`Failed to close a Fastify server, reason: ${reason}`)
-      )
-    )
+    await dbHandler.closeDatabase()
+    await server.close()
+    console.log('Closing Fastify server is done!')
   })
 
   it('should successfully get a empty list of Todos', async () => {
