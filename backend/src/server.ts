bcacb29b4f5973edb4117e2c7468fd5452d89850
yubintw <yubin.cs05g@nctu.edu.tw>
2022-03-02 22:31:11 +0800

    integrate Keycloak as fastify plugin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import { Server, IncomingMessage, ServerResponse } from 'http'
 import path from 'path'
 import { establishConnection } from './plugins/mongodb'
 import { TodoRouter } from './routes/todo'
+import keycloak, { KeycloakOptions } from './plugins/keycloak'
 
 const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
   logger: { prettyPrint: true }
@@ -23,6 +24,15 @@ const startFastify: (port: number) => FastifyInstance<Server, IncomingMessage, S
     root: path.join(__dirname, '../../frontend/build'),
     prefix: '/'
   })
+
+  const opts: KeycloakOptions = {
+    appOrigin: process.env.APP_ORIGIN,
+    keycloakSubdomain: process.env.KEYCLOAK_SUBDOMAIN,
+    clientId: process.env.KEYCLOAK_CLIENT_ID,
+    clientSecret: process.env.KEYCLOAK_CLIENT_SECRET
+  }
+
+  server.register(keycloak, opts)
 
   server.get('/ping', async (request: FastifyRequest, reply: FastifyReply) => {
     return reply.status(200).send({ msg: 'pong' })
