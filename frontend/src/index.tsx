060911dc5a7fd1ea2728f4652921333e7dcfd5ff
yubintw <yubin.cs05g@nctu.edu.tw>
2022-04-01 01:18:22 +0800

    fix code for react 17

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as ReactDOMClient from 'react-dom/client'
 import mockServer from './services/mockService'
 
-const rootContainer = document.getElementById('root')
-if (rootContainer) {
-  const root = ReactDOMClient.createRoot(rootContainer)
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
-  mockServer()
-} else {
-  document.write('cannot find root element')
-}
+mockServer()
