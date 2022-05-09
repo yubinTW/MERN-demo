a455db7752bc89f5ba836544278666e9784f5b82
yubintw <yubin.cs05g@nctu.edu.tw>
2022-05-09 22:12:24 +0800

    fix prettier

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,15 +4,15 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const env = cleanEnv(process.env, {
-    FASTIFY_PORT: port({
-        default: 8888
-    }),
-    FASTIFY_PRETTY_PRINT: bool({
-        default: false
-    }),
-    MONGO_CONNECTION_STRING: url({
-        default: 'mongodb://localhost:27017/myMERN'
-    })
+  FASTIFY_PORT: port({
+    default: 8888
+  }),
+  FASTIFY_PRETTY_PRINT: bool({
+    default: false
+  }),
+  MONGO_CONNECTION_STRING: url({
+    default: 'mongodb://localhost:27017/myMERN'
+  })
 })
 
 export { env }
